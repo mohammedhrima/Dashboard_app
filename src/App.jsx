5eb0536e34dcace7cbc0811a322b1d4fc1fceffa
Mohammed Hrima <mhrima@e1r8p4.1337.ma>
2023-06-08 22:09:50 +0100

    add some pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,34 @@ import List from './pages/list/List'
 import Login from './pages/login/Login'
 import New from './pages/new/New'
 import Single from './pages/single/Single'
+import { userInputs, productInputs } from "./Formsource"
 
 function App() {
   return (
     <div className="App">
-    <Routes>
-      <Route path='*' element={<Home />} />
-      <Route path='/list' element={<List />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/new' element={<New />} />
-      <Route path='/single' element={<Single />} />
-    </Routes>
-  </div>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route
+              path="new"
+              element={<New inputs={userInputs} title="Add New User" />}
+            />
+          </Route>
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":productId" element={<Single />} />
+            <Route
+              path="new"
+              element={<New inputs={productInputs} title="Add New Product" />}
+            />
+          </Route>
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,9 @@
+import Chart from "../../components/chart/Chart"
+import Featured from "../../components/featured/Featured"
+import Navbar from "../../components/navbar/Navbar"
 import Sidebar from "../../components/sidebar/Sidebar"
+import Table from "../../components/table/Table"
+import Widget from "../../components/widget/widget"
 import "./Home.scss"
 
 function Home() {
@@ -6,7 +11,23 @@ function Home() {
         <div className="home" >
             <Sidebar/>
             <div className="homeContainer">
-                container
+                <Navbar/>
+                <div className="widgets">
+                    <Widget type="user"/>
+                    <Widget type="order"/>
+                    <Widget type="earning"/>
+                    <Widget type="balance"/>
+                </div>
+                <div className="charts">
+                    <Featured/>
+                    <Chart/>
+                </div>
+                <div className="listContainer">
+                    <div className="listTitle">
+                        Latest Transactions
+                    </div>
+                    <Table/>
+                </div>
             </div>
         </div>
     )
